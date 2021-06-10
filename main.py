87bd260b6261f7fa81fd74d58eeefdfb07e34e7f
Jinyong Eom <46102597+wlsdyd7511@users.noreply.github.com>
2021-06-11 00:10:14 +0900

    Make clear scene
    Make player die when collapse with traps
    Move to clear map when clear all stage

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import sys
 pygame.init()
 pygame.display.set_caption('Songjun')
 bg_color = [2, 64, 77]
+END_LEVEL = 2
 
 
 def main():
-    if len(sys.argv) > 1:
-        with open('map/map2 숫자.txt', 'r') as f:
-            map = [list(n.rstrip('\n')) for n in f.readlines()]
-    else:
-        with open('map/map1 숫자.txt', 'r') as f:
-            map = [list(n.rstrip('\n')) for n in f.readlines()]
+    level = 1
+    current_level = 1
+    with open('map/map1 숫자.txt', 'r') as f:
+        map = [list(n.rstrip('\n')) for n in f.readlines()]
     width = len(map[0])
     height = len(map)
     screen = pygame.display.set_mode((width*16, height*16))
@@ -25,6 +24,7 @@ def main():
     position = [50, 50]
     spd = [0, 0]
     jump_spd = -7
+    MAX_SPD = 1.5
     gravity = 0.4
     is_bottom = False
     is_right = False
@@ -41,16 +41,23 @@ def main():
     flag = pygame.image.load('image/flag/flag.png')
     flag = pygame.transform.scale(flag, (16, 32))
     fake = pygame.image.load('image/flag/flag fake/flag fake.png')
+    fake = pygame.transform.scale(fake, (16, 32))
     portal = pygame.image.load('image/portal/portal.png')
     portal = pygame.transform.scale(portal, (32, 32))
-    for h in range(height):
-        for w in range(width):
-            if map[h][w] == '7':
-                position = [w*16, h*16]
-   
+    position = spawn(height, width, map)
 
 
     while True:
+        if level != current_level:
+            if current_level == END_LEVEL:
+                with open('map/clear.txt', 'r') as f:
+                    map = [list(n.rstrip('\n')) for n in f.readlines()]
+            else:
+                with open(f'map/map{level} 숫자.txt', 'r') as f:
+                    map = [list(n.rstrip('\n')) for n in f.readlines()]
+            position = spawn(height, width, map)
+            current_level = level
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -64,11 +71,15 @@ def main():
                     if is_bottom:
                         spd[1] = jump_spd
                         is_bottom = False
+                elif event.key == pygame.K_LSHIFT:
+                    MAX_SPD = 4
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     is_left = False
                 elif event.key == pygame.K_RIGHT:
                     is_right = False
+                elif event.key == pygame.K_LSHIFT:
+                    MAX_SPD = 2
         if not is_bottom:
             spd[1] += gravity
 
@@ -87,20 +98,35 @@ def main():
         elif direction < 0:
             if spd[0] > 0:
                 spd[0] -= 0.4
-            elif spd[0] > -1.3:
+            elif spd[0] > -MAX_SPD+0.2:
                 spd[0] -= 0.2
             else:
-                spd[0] = -1.5
+                spd[0] = -MAX_SPD
         else:
             if spd[0] < 0:
                 spd[0] += 0.4
-            elif spd[0] < 1.3:
+            elif spd[0] < MAX_SPD-0.2:
                 spd[0] += 0.2
             else:
-                spd[0] = 1.5
+                spd[0] = MAX_SPD
 
         position[0] += spd[0]
         position[1] += spd[1]
+        print(position)
+        print(width)
+        print()
+
+        if position[0] <= 1:
+            position[0] = 1
+            if spd[0] < 0:
+                spd[0] = 0
+        elif position[0] >= (width * 16) - 31:
+            position[0] = (width * 16) - 31
+            if spd[0] > 0:
+                spd[0] = 0
+
+        if position[1] > (height * 16) - 16:
+            spawn(height, width, map)
 
         screen.fill(tuple(bg_color))
         for h in range(height):
@@ -162,16 +188,44 @@ def main():
             if spd[0] < 0:
                 spd[0] = 0
                 position[0] = (char_block[0]+1) * 16
-        if map[int(char_block[1])][int(char_block[0])] == '5':
-            level()
 
+        touched_block = []
+        touched_block.append(map[int(position[1]/16)][int(position[0]/16)])
+        touched_block.append(map[int((position[1]+15)/16)][int(position[0]/16)])
+        touched_block.append(map[int(position[1]/16)][int(((position[0]+15)+15)/16)])
+        touched_block.append(map[int((position[1]+15)/16)][int(((position[0]+15)+15)/16)])
+
+        touched_spike = []
+        touched_spike.append(map[int(position[1]/16)][int(position[0]/16)])
+        touched_spike.append(map[int((position[1]+2)/16)][int(position[0]/16)])
+        touched_spike.append(map[int(position[1]/16)][int(((position[0]+15)+15)/16)])
+        touched_spike.append(map[int((position[1]+2)/16)][int(((position[0]+15)+15)/16)])
+
+        if '5' in touched_block:
+            level += 1
+        
+        if '4' in touched_block or '6' in touched_block or '3' in touched_spike:
+            position = spawn(height, width, map)
 
         pygame.display.update()
         fps.tick(60)
 
 def level():
+    print('level')
     with open('map/map2 숫자.txt', 'r') as f:
             map = [list(n.rstrip('\n')) for n in f.readlines()]
+
+
+def death():
+    print('death')
+
+def spawn(height, width, map):
+    porition = [0, 0]
+    for h in range(height):
+        for w in range(width):
+            if map[h][w] == '7':
+                position = [w*16, h*16]
+    return position
 
 
 
